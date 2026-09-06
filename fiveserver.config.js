60d9733167efa2a79ba3cb752d0d14391f75c27a
simon <simon.reedtz-hansen@utb.ecutbildning.se>
2026-09-06 22:03:01 +0200

    fixed dark mode for tablet and desktop dimensions

---
+++ fiveserver.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // highlight: true, // enable highlight feature
+  injectBody: true, // enable instant update
+  remoteLogs: true, // enable remoteLogs
+  remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+  navigate: true, // enable auto-navigation
+};
